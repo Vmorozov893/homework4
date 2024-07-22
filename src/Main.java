8edd7959eafb88b01e435b4e9955c9f691a953c5
vmorozov893 <vmorozov893@gmail.com>
2024-07-22 03:19:21 +0300

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Задание 1\n");
+
+        byte clientOS = 0;
+        switch (clientOS){
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке\n");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке\n");
+                break;
+        }
+
+
+
     }
 }
